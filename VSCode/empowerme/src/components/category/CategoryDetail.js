7d7a51ab301c67c750cfc7749c26b633abcf2d3d
ChrD2022 <103274391+ChrD2022@users.noreply.github.com>
2023-04-18 13:10:31 +0100

    Fetching

---
+++ VSCode/empowerme/src/components/category/CategoryDetail.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const CategoryDetail = () => {
+    return (
+    <div>
+
+        CategoryDetail
+        
+    </div>
+    )
+}
+
+export default CategoryDetail
--- VSCode/empowerme/src/containers/CategoryContainer.js
+++ VSCode/empowerme/src/containers/CategoryContainer.js
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 const CategoryContainer = () => {
+    const url = "http://localhost:8080/api/categories"
+    const [categories, setCategories] = useState ([]);
+
+    const getCategories = () => {
+        fetch(url)
+        .then(results => results.json())
+        .then(categoryData => setCategories(categoryData))
+    }
+
+    useEffect(() => {
+        getCategories();
+    },[])
+
     return (
-    <div>CategoryContainer</div>
+    <div>
+        CategoryContainer
+    </div>
     )
 }
 
