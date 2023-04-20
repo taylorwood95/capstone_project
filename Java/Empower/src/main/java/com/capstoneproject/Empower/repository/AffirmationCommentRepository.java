dfbe2b1eabc37ae4336e0360eeaa0a2a4905bc42
taylorwood95 <115211674+taylorwood95@users.noreply.github.com>
2023-04-20 14:57:11 +0100

    work in progress. Affirmations needs to be changed

--- Java/Empower/src/main/java/com/capstoneproject/Empower/repository/AffirmationCommentRepository.java
+++
@@ -1,9 +0,0 @@
-package com.capstoneproject.Empower.repository;
-
-import com.capstoneproject.Empower.models.AffirmationComment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface AffirmationCommentRepository extends JpaRepository<AffirmationComment, Long> {
-}
